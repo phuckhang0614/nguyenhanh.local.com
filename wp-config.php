0765b9d13cd25db92f529e6e0c870d6d0e12750e
phuckhang0614 <phuckhangne204@gmail.com>
2024-12-02 11:03:48 +0700

    Push Code
    Push Code

---
+++ wp-config.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the installation.
+ * You don't have to use the website, you can copy this file to "wp-config.php"
+ * and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * Database settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://developer.wordpress.org/advanced-administration/wordpress/wp-config/
+ *
+ * @package WordPress
+ */
+
+// ** Database settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'nguyenhanh.local.com' );
+
+/** Database username */
+define( 'DB_USER', 'root' );
+
+/** Database password */
+define( 'DB_PASSWORD', '990614' );
+
+/** Database hostname */
+define( 'DB_HOST', 'localhost:3316' );
+
+/** Database charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8mb4' );
+
+/** The database collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+/**#@+
+ * Authentication unique keys and salts.
+ *
+ * Change these to different unique phrases! You can generate these using
+ * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
+ *
+ * You can change these at any point in time to invalidate all existing cookies.
+ * This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         'v>T{_-vw7)VJ22#24,z%&`i<=;) K%@z-2x(V`)YT%uI/j=ZF]bt?re]>wk?VxC.' );
+define( 'SECURE_AUTH_KEY',  '{r+NN-)OHXtl=O:c0B{Y,@ 1R}g~j{r*aV0eo8-~}j^!J%;8gT753E0sKfEU%o(P' );
+define( 'LOGGED_IN_KEY',    'mec|5u2[/BgJ=mv%s*Y)p>$bC |<.mcGDa=JN*N~BOq[I7*.iD0tJGX!%)L+$Ps!' );
+define( 'NONCE_KEY',        'BA5Tf=v<reoew^J:B?S~q^]&=;EkU9P2R<kN 4;bjwub<:Vy]0bn9Kv9E@fok2.z' );
+define( 'AUTH_SALT',        'yz-z2&X9`k9s5O7[b@W]K?jB-[RIfx ;rJ6}yk</=%y.}e4H/,-[Gkq4a3Ovx}7g' );
+define( 'SECURE_AUTH_SALT', 'Q;ii2+OcL)YRXX2o|vW UwDT<paNRz>X>|d5PF8px2oD?e$?RXOdsrDmL{~=v02F' );
+define( 'LOGGED_IN_SALT',   'fA9hHv#UB@7ins!7u6HKjTClK{^#jpk!;nn9Pg04N&flrz6$gMU!H>^9.68a7[m.' );
+define( 'NONCE_SALT',       '$!X7Y6|m>6FOIwg fMBg8j@n<l|koC0Kzm;MHZm#-:5]*-e W#!|%rNQ[D/%$`|o' );
+
+/**#@-*/
+
+/**
+ * WordPress database table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://developer.wordpress.org/advanced-administration/debug/debug-wordpress/
+ */
+define( 'WP_DEBUG', false );
+
+/* Add any custom values between this line and the "stop editing" line. */
+
+
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
---
+++ wp-content/themes/essentials/single.php
@@ -0,0 +1,231 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package essentials
+ */
+
+get_header();
+
+$classes = '';
+$styles = '';
+
+if(get_post_type()=='post'){
+ if(!empty(pix_get_option('blog-bg-color'))){
+ 	if(pix_get_option('blog-bg-color')=='custom'){
+ 		$styles = 'background:'.pix_get_option('custom-blog-bg-color').';';
+        $classes = '';
+ 	}else{
+ 		$classes = 'bg-'.pix_get_option('blog-bg-color'). ' ';
+ 	}
+ }
+}else{
+ if(!empty(pix_get_option('pages-bg-color'))){
+ 	if(pix_get_option('pages-bg-color')=='custom'){
+ 		$styles = 'background:'.pix_get_option('custom-pages-bg-color').';';
+ 	}else{
+ 		$classes = 'bg-'.pix_get_option('pages-bg-color'). ' ';
+ 	}
+ }
+}
+
+$add_intro_placeholder = false;
+if(get_post_type()=='post'){
+    $post_intro = false;
+    if(!empty(pix_get_option('post-with-intro'))&&pix_get_option('post-with-intro')){
+        $post_intro = true;
+    }
+
+    if(!empty($_GET["post_intro"])){
+        switch ($_GET["post_intro"]) {
+            case 'true':
+                $post_intro = true;
+                break;
+            case 'false':
+                $post_intro = false;
+                break;
+        }
+    }
+    if($post_intro){
+        get_template_part( 'template-parts/intro' );
+    }else{
+        $add_intro_placeholder = true;
+    }
+}else{
+    get_template_part( 'template-parts/intro' );
+}
+
+
+if(!get_post_meta( get_the_ID(), 'pix-hide-top-padding', true )){
+		$classes .= 'pix-pt-20';
+}
+
+$containerClass = 'container';
+if(get_post_type()=='post' && !empty(pix_get_option('blog-full-width-layout'))){
+	$containerClass = 'container-fluid';
+}
+
+?>
+
+<div id="content" class="site-content <?php echo esc_html( $classes );?>" style="<?php echo esc_html( $styles ); ?>" >
+	<div class="<?php echo esc_attr($containerClass); ?>">
+		<div class="row">
+
+			<?php
+
+            if($add_intro_placeholder){
+                ?>
+                <div class="pix-main-intro-placeholder"></div>
+                <?php
+            }
+
+            $blog_layout = 'default';
+            if(!empty(pix_get_option('blog-layout'))){
+                $blog_layout = pix_get_option('blog-layout');
+            }
+            if(!empty($_GET["blog_layout"])){
+                switch ($_GET["blog_layout"]) {
+                    case 'default':
+                        $blog_layout = 'default';
+                        break;
+                    case 'right-sidebar':
+                        $blog_layout = 'right-sidebar';
+                        break;
+                    case 'left-sidebar':
+                        $blog_layout = 'left-sidebar';
+                        break;
+                }
+            }
+			while ( have_posts() ) :
+				the_post();
+                if(get_post_type()=='post'){
+                    switch ($blog_layout) {
+                        case 'left-sidebar':
+                            get_template_part( 'template-parts/content', 'post-sidebar' );
+                            break;
+                        case 'right-sidebar':
+                            get_template_part( 'template-parts/content', 'post-sidebar' );
+                            break;
+                        case 'default-normal':
+                            get_template_part( 'template-parts/content', 'post-normal' );
+                            break;
+                        default:
+                            get_template_part( 'template-parts/content', 'post' );
+                    }
+                }elseif (get_post_type() == 'elementor_library') {
+                    // Elementor template page
+                    ?>
+                    <div class="col-12 col-md-10 offset-md-1">
+                		<div id="primary" class="content-area">
+                			<main id="main" class="site-main">
+                				<article id="post-<?php the_ID(); ?>">
+                                    <?php
+                                        get_template_part( 'template-parts/content', 'page' );
+                                    ?>
+                                </article>
+                            </main>
+                        </div>
+                    </div>
+                <?php
+            }elseif (get_post_type() == 'search') {
+                get_template_part( 'template-parts/content', 'search' );
+            }elseif (get_post_type() == 'none') {
+                get_template_part( 'template-parts/content', 'none' );
+            }else{
+                ?>
+				<div class="col-12">
+				<?php
+    	            get_template_part( 'template-parts/content', 'page' );
+				?>
+				</div>
+				<?php
+            }
+			endwhile;
+			?>
+        </div>
+
+ <!-- Begin Post related -->
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-12">
+                    <h2 class="nguyenhanh-post_related_heading">Bài viết liên quan</h2> 
+                </div>
+                <?php
+                    $html = '';
+                    // Lấy Danh mục của bài viết hiện tại
+                    $categories = get_the_category(get_the_ID());
+                    if ($categories) :
+                        $category_ids = array();
+                        foreach($categories as $individual_category) $category_ids[] = $individual_category->term_id;
+                    // $relatedposts = new WP_Query( array( 'post_type' => 'post', 'posts_per_page' => '3', 'post__not_in' => array(get_the_ID()), 'category__not_in' => 20 ) );
+                        $relatedposts = new WP_Query( array( 'post_type' => 'post', 'posts_per_page' => '3','category__in' => $category_ids, 'post__not_in' => array(get_the_ID()) ) );
+                        if ( $relatedposts->have_posts() ) :
+                            while ( $relatedposts->have_posts() ) : 
+                                $relatedposts->the_post();
+                                if( $currentProductId != get_the_ID() ) :
+                                    $html .= '<div class="col-lg-4 col-md-6 col-12 pix-pt-10">';
+                                    $html .= '<article class="nguyenhanh_post-related-article" >';
+                                    //
+                                    $postId = get_the_ID();
+                                    $postTitle = get_the_title();
+                                    $postUrl = get_permalink();
+                                    $postAvatar = get_the_post_thumbnail_url();
+                                    $postExcerpt = get_the_excerpt();
+                                    //
+                                    $featured_image_full = get_the_post_thumbnail($postId, 'full', array('class' => 'attachment-full size-full'));
+                                    //
+                                    $html .= '<div class="nguyenhanh-post_related_card">';
+                                    $html .= '<a class="nguyenhanh-post_related-thumbnail-link" href="'.$postUrl.'">';
+                                    $html .= '<div class="nguyenhanh-post_related-thumbnail-img">';
+                                    $html .= $featured_image_full;
+                                    $html .= '</div>';
+                                    //
+                                    // $terms = get_the_terms($postId, 'price_nhom' );
+                                    // foreach ( $terms as $term ) {
+                                    //     $html .= '<div class="nguyenhanh_post_related-brand">';
+                                    //     $html .= $term->name;
+                                    //     $html .= '</div>';
+                                    // }
+                                    $html .= '</a>';
+                                    //
+                                    $html .= '<div class="nguyenhanh_post_related-text">';
+                                    $html .= '<h3 class="nguyenhanh_post_related-title">';
+                                    $html .= '<a href="'.$postUrl.'" alt="'.$postTitle.'" title="'.$postTitle.'">';
+                                    $html .= $postTitle;
+                                    $html .= '</h3>';
+                                    $html .= '</a>';
+                                    //
+
+                                    // giới hạn từ hiển thị trên excerpt
+                                    $excerpt = get_the_excerpt();
+                                    $postExcerpt = wp_trim_words($excerpt, 30); // Đặt số từ mong muốn, ví dụ: 30 từ
+                                    //
+
+                                    $html .= '<div class="nguyenhanh_post_related-excerpt">';
+                                    $html .= '<p>';
+                                    $html .= $postExcerpt;
+                                    $html .= '</p>';
+                                    $html .= '</div>';
+                                    $html .= '<a class="nguyenhanh-post_related-read-more" href="'.$postUrl.'" alt="'.$postTitle.'" title="'.$postTitle.'">';
+                                    $html .= 'Xem thêm';
+                                    $html .= '</a>';
+                                    $html .= '</div>';
+                                    //
+                                    $html .= '</div>';
+                                    $html .= '</article>';
+                                    $html .= '</div>';
+                                endif;
+                            endwhile;
+                            wp_reset_query();
+                        endif;
+                    endif;
+                    echo $html;
+                ?>
+            </div>
+        <!-- End Post content -->
+
+    </div>
+</div>
+<?php
+get_footer();
